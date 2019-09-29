9f2fd9a47a84f88bbfd5e26cfa381fe1871f8dbf
David Chavez <david@dcvz.io>
2019-09-29 13:25:20 +0200

    Add Xcode project for core framework

---
+++ PSPDeltaCore/PSPDeltaCore/PSPDeltaCore.h
@@ -0,0 +1,19 @@
+//
+//  PSPDeltaCore.h
+//  PSPDeltaCore
+//
+//  Created by David Chavez on 29.09.19.
+//  Copyright © 2019 David Chavez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PSPDeltaCore.
+FOUNDATION_EXPORT double PSPDeltaCoreVersionNumber;
+
+//! Project version string for PSPDeltaCore.
+FOUNDATION_EXPORT const unsigned char PSPDeltaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSPDeltaCore/PublicHeader.h>
+
+
